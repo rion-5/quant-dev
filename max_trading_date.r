41cafd6acc9be41f4388db70e649ee1353d22ca7
rion-5 <sklee@hanyang.ac.kr>
2024-07-07 16:04:37 +0900

    add host

--- max_trading_date.r
+++ max_trading_date.r
@@ -1,10 +1,10 @@
 library(DBI)
 library(RPostgres)
 
-con <-dbConnect(RPostgres::Postgres(), user="quant", password="quant14", dbname="quantdb",port="8080")
+con <-dbConnect(RPostgres::Postgres(),host="localhost", user="quant", password="quant14", dbname="quantdb",port="8080")
 query <- ("SELECT MAX(TRADING_DATE) FROM STOCK;")
 
 df <- dbGetQuery(con, query)
 print(df)
 
-dbDisconnect(con);
+dbDisconnect(con)
